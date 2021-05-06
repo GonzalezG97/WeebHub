d488710395670f152909dd729362e7f041ed293a
Hagen DeRouen <hagend87@gmail.com>
2021-05-05 19:49:15 -0500

    Add anime api starting  routes. Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-// const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -18,19 +18,12 @@ const handlebars = require('express-handlebars').create({
     extname: 'hbs'
 });
 
-
-
-
 app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/bootstrap', express.static((__dirname + '/node_modules/bootstrap/dist')));
-app.use(require('./controllers/routes/anime_routes'))
 
-
-// app.get('/', function(req, res) {
-//     res.render('index');
-// });
+app.use(routes);
 
 sequelize.sync().then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));

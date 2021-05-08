fa3f909bfc3838e9f2838e64502b0f7b0e12805e
ward438 <75048548+ward438@users.noreply.github.com>
2021-05-08 15:50:45 -0500

    free the slug

--- models/index.js
+++ models/index.js
@@ -4,32 +4,6 @@ const User = require('./User');
 const AnimeTitle = require('./AnimeTitle');
 const PosterImage = require('./PosterImage');
 
-// Anime.hasMany(Review, {
-//     foreignKey: 'anime_id'
-// });
-// Review.belongsTo(Anime);
-
-// Anime.hasOne(AnimeTitle);
-// AnimeTitle.belongsTo(Anime);
-
-// // AnimeTitle.hasOne(Review);
-// // Review.belongsTo(AnimeTitle);
-// // Review.hasOne(AnimeTitle);
-// // AnimeTitle.belongsTo(Review);
-
-// Anime.hasOne(PosterImage);
-// PosterImage.belongsTo(Anime);
-
-// User.hasMany(Review);
-// Review.belongsTo(User, {
-//     foreignKey: 'user_id'
-// });
-
-// const Anime = require('./Anime');
-// const Review = require('./Review');
-// const User = require('./User');
-// const AnimeTitle = require('./AnimeTitle');
-// const PosterImage = require('./PosterImage');
 
 Review.belongsTo(Anime);
 
